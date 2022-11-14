66b1d33ae641255158b9dccdd583fc8b0627ec47
muhammedsaamy <muhammedsaamy@gmail.com>
2022-11-14 13:54:30 +0200

    login form with validation

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, observable } from 'rxjs';
-import {RegisterRequest} from './register-request'
+import {RegisterRequest} from './register-request';
+import {LoginRequest} from './login-request';
+
 
 
 @Injectable({
@@ -16,5 +18,11 @@ signUp(registerData:RegisterRequest):Observable<any>
 {
   return this.http.post('https://routeegypt.herokuapp.com/signup',registerData)
 }
+
+signIn(loginData:LoginRequest):Observable<any>
+{
+  return this.http.post('https://routeegypt.herokuapp.com/signin',loginData)
+}
+
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { LoginRequest } from '../login-request';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _AuthService:AuthService, private _router:Router) { }
 
   ngOnInit(): void {
   }
+
+  submitForm(){
+    if(this.loginForm.invalid){return;}
+    this._AuthService.signIn(this.loginForm.value).subscribe((data)=>{
+      if(data.message=="success"){
+        alert(data.message)
+        this._router.navigateByUrl("/home")
+      }
+      else{
+        alert(data.message)
+      }
+    })
+
+  }
+
+  loginForm:FormGroup=new FormGroup({
+    "email":new FormControl(null,[Validators.required,Validators.email]),
+    "password":new FormControl(null,[Validators.required,Validators.pattern(/^[a-zA-Z][0-9]{3}$/)])
+  })
+
 
 }
