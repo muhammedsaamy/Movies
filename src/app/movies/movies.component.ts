858074cf4bc65a9360588e7fce6c3c1f1c09791b
muhammedsaamy <muhammedsaamy@gmail.com>
2022-11-18 04:42:22 +0200

    added pagination to movies component

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -12,20 +12,41 @@ export class MoviesComponent implements OnInit {
   moviesList:any[]=[];
   tvShowsList:any[]=[];
   imgBaseurl:string="https://image.tmdb.org/t/p/original";
-
+  pageNum:number=1
   constructor(private _trendingService:TrendingService, private _NgxSpinnerService:NgxSpinnerService) { }
-
-  ngOnInit(): void {
-    this.getMovies();
-  }
-
-  getMovies(){
-    this._NgxSpinnerService.show();
-    this._trendingService.getTrending("movie").subscribe((response)=>{
+  getPaginatedData(pageNumber:number){
+    this.pageNum=pageNumber;
+    this._trendingService.getTrendingPaginatedItems('movie',pageNumber).subscribe((response)=>{
       this.moviesList= response.results;
     },(error)=>{alert(error)},()=>{
       this._NgxSpinnerService.hide();
     })
+  }
+  prev(){
+    if(this.pageNum>1){
+    this.pageNum--;
+    this.getPaginatedData(this.pageNum)
     }
+  }
+  next(){
+    if(this.pageNum<5){
+      this.pageNum++;
+      this.getPaginatedData(this.pageNum)
+      }
+    }
+
+  ngOnInit(): void {
+    this.getPaginatedData(1);
+    // this.getMovies();
+  }
+
+  // getMovies(){
+  //   this._NgxSpinnerService.show();
+  //   this._trendingService.getTrending("movie").subscribe((response)=>{
+  //     this.moviesList= response.results;
+  //   },(error)=>{alert(error)},()=>{
+  //     this._NgxSpinnerService.hide();
+  //   })
+  //   }
 
 }
--- src/app/trending.service.ts
+++ src/app/trending.service.ts
@@ -10,6 +10,13 @@ export class TrendingService {
 
   constructor(private http:HttpClient) { }
 
+
+
+  getTrendingPaginatedItems(mediaType:any , pageNumber:number):Observable<any>
+  {
+  return this.http.get(`https://api.themoviedb.org/3/discover/${mediaType}?api_key=c636ed7787cc302d96bf88ccf334e0d8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber}&with_watch_monetization_types=flatrate`)
+  }
+
   getTrending(mediaType:any):Observable<any>
   {
   return this.http.get(`https://api.themoviedb.org/3/trending/${mediaType}/day?api_key=c636ed7787cc302d96bf88ccf334e0d8`)
